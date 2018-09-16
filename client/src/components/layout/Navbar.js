037d6f59f6cf33ef379a2b5e3717a68b5d1e161f
yourfavoritedev <ChristopherNgo94@gmail.com>
2018-09-16 15:05:14 -0700

    moved in-line styles of fonts to css. testing render speed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -59,7 +59,7 @@ class Navbar extends React.Component{
 		)		
 
 		return(
-			<div>
+			<div className="navigation">
 			  <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
 			    <div className="container">
 			      <Link className="navbar-brand" to="/"><img src={sonyUniverseLogo} className="logo"/></Link>
