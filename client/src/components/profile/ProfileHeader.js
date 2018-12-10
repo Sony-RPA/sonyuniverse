765b7b844d8079789db8c6b6171587eb93bfddc7
yourfavoritedev <ChristopherNgo94@gmail.com>
2018-12-09 16:48:15 -0800

    Fixed display size of profile image on profile component

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -10,7 +10,7 @@ class ProfileHeader extends React.Component{
               <div className="card card-body bg-info text-white mb-3 shadow">
                 <div className="row">
                   <div className="col-4 col-md-3 m-auto">
-                    <img className="rounded-circle" src={profile.user.avatar} alt="" />
+                    <img className="rounded-circle" src={profile.user.avatar} style={{ height: "200px", width: "200px" }} alt="" />
                   </div>
                 </div>
                 <div className="text-center">
